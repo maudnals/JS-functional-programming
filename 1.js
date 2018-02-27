d513027cf260c6d721fd407537bbd662cc6a4fc9
maudnals <maud.nalpas@gmail.com>
2018-02-27 08:43:56 +0100

    pure function

--- 1.js
+++ 1.js
@@ -17,4 +17,45 @@ myArray.push("another one", "bla", "whatever");
 // remove 2 elements from position 0 and insert jo maud scar from position 0
 myArray.splice(0, 2, "jo", "maud", "scar");
 
-console.log(a);
+
+/**
+ * 
+ * observable change = side effect (visible from outside)
+ */
+
+/**
+ * PURE FUNCTION
+ */
+
+(function() {
+
+  let players = ["Arya", "Daenerys", "Tyrion", "Catelyn"];
+  let lives = 3;
+
+  function impure_playerLost() {
+    // always returns something different => not referentially transparent
+    lives--;
+  }
+
+  function impure_addPlayer(name) {
+    // mutator method
+    players.push(name);
+  }
+
+  impure_playerLost();
+  impure_addPlayer("Maud");
+
+
+  players = ["Arya", "Daenerys", "Tyrion", "Catelyn"];
+  lives = 3;
+
+  function pure_playerLost(lives) {
+    // always same output for same input => referentially transparent
+    return --lives;
+  }
+
+  // note how with FP, function outputs must be assigned: since side effects are forbidden, a function call in itself "does nothing"
+  lives = pure_playerLost(lives);
+
+
+})()
